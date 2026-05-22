520839c89111693cf612a00512b0d28b7d5c40ff
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-22 14:12:56 -0600

    feat:agregamos  las reglas de validacion de la entidad.

---
+++ Seph.Principal.Application/Features/Auth/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Application.Features.Auth.DTOs;
+using Seph.Principal.Domain.Entities;
+using Seph.Principal.Domain.Repositories;
+using System.Net;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.Login
+{
+    public sealed class LoginCommandHandler(IIdentityService identityService,IJwtTokenService jwtTokenService,IRefreshTokenSessionRepository sessionRepository,
+        IUnitOfWork unitOfWork) : IRequestHandler<LoginCommand, ResponseWrapper<AuthResponseDto>>
+    {
+
+        public async Task<ResponseWrapper<AuthResponseDto>> Handle(LoginCommand request, CancellationToken cancellationToken) {
+            /*1. Validate the user's credentials using the IIdentityService.*/
+            var user = await identityService.ValidateCredentialAsync(request.Email, request.Password,cancellationToken);
+            if (user is null)
+            {
+                return ResponseFactory.Failure<AuthResponseDto>("Crendeicales invalidas",HttpStatusCode.Unauthorized);
+                
+            }
+            /* refrescamos el token de acceso y el token de refresco utilizando el IJwtTokenService. */
+            var refreshToken = jwtTokenService.CreateRefreshToken();
+            /*creamos el hash del token de refresco */
+            var refreshTokenHash = jwtTokenService.HashRefreshToken(refreshToken);
+            /*generamos la fecha de expiración del token de refresco  7 dias*/
+            var expiresAtUtc = DateTimeOffset.UtcNow.AddDays(7);
+            /*generamos la sesión del token de refresco */
+            var session = RefreshTokenSession.Create(user.Id,refreshTokenHash,request.DeviceId,request.IpAddress,expiresAtUtc);
+            /*agregamos la sesión del token de refresco al repositorio */
+            await sessionRepository.AddAsync(session, cancellationToken);
+            /*marcamos la última vez que el usuario inició sesión */
+            await identityService.MarkLastLoginAsync(user.Id, cancellationToken);
+            /*generamos el token de acceso */
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            /*generamos el token de acceso */
+            var accessToken = jwtTokenService.CreateAccessToken(user);
+            /*creamos la respuesta  con los tokens y la información del usuario */
+            var response = new AuthResponseDto(
+                accessToken, refreshToken, DateTimeOffset.UtcNow.AddMinutes(15),
+                new UserSessionDto(user.Id, user.Email, user.FullName, user.Roles, user.Permissions)
+                );
+            /*regresamos la respuesta  que es una entidad del tipo responseWrapper*/
+            return ResponseFactory.Success(response,"Autenticación exitosa");
+        }
+    }
+
+
+}
---
+++ Seph.Principal.Application/Features/Auth/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.Login
+{
+    /*Esta clase es un validador para el comando de inicio de sesión (LoginCommand) utilizando FluentValidation. 
+     * Actualmente, no se han definido reglas de validación específicas, pero esta clase se puede expandir en el 
+     * futuro para incluir validaciones como verificar que el correo electrónico tenga un formato válido o que la 
+     * contraseña cumpla con ciertos requisitos de seguridad. */
+    public sealed class LoginCommandValidator: AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            /*Aquí es donde se pueden agregar las reglas de validación para el comando de inicio de sesión. 
+            * Por ejemplo, se podrían agregar reglas para validar que el correo electrónico tenga un formato válido 
+            * o que la contraseña cumpla con ciertos requisitos de seguridad. */
+            RuleFor(request => request.Email).NotEmpty().EmailAddress().MaximumLength(256);
+            RuleFor(request => request.Password ).NotEmpty().MinimumLength(8).MaximumLength(128);
+            RuleFor(request => request.DeviceId).NotEmpty().MaximumLength(128);
+            RuleFor(request => request.IpAddress).NotEmpty().MaximumLength(64);
+        }
+    }
+}
