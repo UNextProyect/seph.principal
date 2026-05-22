2c2c855152e719bbcf9ef2c44441680f04bc3eed
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-22 11:42:50 -0600

    feat:agregamos las interfaces que se encuentran dentro de la aplicacion  o capa de aplicacion.

---
+++ Seph.Principal.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seph.Principal.Domain.Entities;
+
+namespace Seph.Principal.Application.Common.Interfaces
+{
+    /*Esta interfaz define el contrato para la capa de acceso a datos de la aplicación.*/
+    public interface IApplicationDbContext
+    {
+        DbSet<RefreshTokenSession> RefreshTokenSessions { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+
+}
---
+++ Seph.Principal.Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        Guid? UserId { get; set; }
+        string? Email { get; set; }
+
+        string? IpAddress { get; set; }
+
+        string? DeviceId { get; set; } 
+    }
+}
