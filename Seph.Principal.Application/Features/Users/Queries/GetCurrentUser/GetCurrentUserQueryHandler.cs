4e59a33163cb727ddb0647fffe8640b2fa625e8f
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 01:48:15 -0600

    fix:se agrega el metodo para consultar al usuario por identificador

---
+++ Seph.Principal.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Application.Features.Auth.DTOs;
+
+namespace Seph.Principal.Application.Features.Users.Queries.GetCurrentUser
+{
+    /*Este código define un manejador de consultas para obtener la información del usuario actual.*/
+    public sealed class GetCurrentUserQueryHandler(IIdentityService identityService)
+        : IRequestHandler<GetCurrentUserQuery,ResponseWrapper<UserSessionDto>>
+    {
+        /*El método Handle es el encargado de procesar la consulta GetCurrentUserQuery.*/
+        public async Task<ResponseWrapper<UserSessionDto>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await identityService.GetUserByIdAsync(request.UserId,cancellationToken);
+            if (user is null)
+            {
+                return ResponseFactory.Failure<UserSessionDto>("Usuario no encontrado",System.Net.HttpStatusCode.NotFound)!;
+            }
+            /*Si el usuario es encontrado, se crea un objeto UserSessionDto con la información del usuario
+             * y se devuelve una respuesta exitosa con ese objeto como datos.*/
+            return ResponseFactory.Success(new UserSessionDto(
+
+                user.Id,
+                user.Email,
+                user.FullName,
+                user.Roles,
+                user.Permissions), "Usuario actual obtenido correctamente");
+            
+        }
+    }
+}
