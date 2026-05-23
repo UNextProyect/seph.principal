823a3b6917084d8b93eb1a254adca91f76f8043d
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 02:14:04 -0600

    fix:se genera la concentracion del dependencyInjection

---
+++ Seph.Principal.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Seph.Principal.Application.Common.Behaviors;
+using System.Reflection;
+
+namespace Seph.Principal.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            return services;
+            
+        }
+    }
+}
