855f7e211f2f4fcbb2473b0fdbd8cc7635f51506
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 00:11:05 -0600

    feat:agregamos el record  que se encarga de refrescar el toquen a traves del command

---
+++ Seph.Principal.Application/Features/Auth/Commands/RefreshToken/RefreshTokenCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Application.Features.Auth.DTOs;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.RefreshToken
+{
+    public sealed record RefreshTokenCommand(string RefreshToken,string DeviceId,string IpAddress):
+        IRequest<ResponseWrapper<AuthResponseDto>>;
+
+
+}
