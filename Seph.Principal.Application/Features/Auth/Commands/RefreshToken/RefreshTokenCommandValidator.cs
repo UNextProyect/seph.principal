c7f580c2425052455d74a62397362aaef51cc9ee
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 01:12:31 -0600

    feat:se agrega el refrshtoken que se encarga de ser validado

---
+++ Seph.Principal.Application/Features/Auth/Commands/RefreshToken/RefreshTokenCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.RefreshToken
+{
+    public sealed class RefreshTokenCommandValidator: AbstractValidator<RefreshTokenCommand>
+    {
+        public RefreshTokenCommandValidator()
+        {
+            RuleFor(request => request.RefreshToken).NotEmpty().MinimumLength(32);
+            RuleFor(request => request.DeviceId).NotEmpty().MaximumLength(128);
+            RuleFor(request => request.IpAddress).NotEmpty().MaximumLength(64);
+        }
+
+    }
+}
