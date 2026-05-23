f9b2cf1269687624d131ba4e1efcd92759c0cf16
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 02:28:52 -0600

    feat:se agrega la estructura base de toda la arquitectura del proyecto.

---
+++ Seph.Principal.Infraestructure/Authentication/JwtOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Infraestructure.Authentication
+{
+    public sealed class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+
+        public string Issuer { get; init; } = string.Empty;
+        public string Audience { get; init; } = string.Empty;
+        public string SigningKey { get; init; } = string.Empty;
+        public int AccessTokenMinutes { get; init; } = 15;
+    }
+
+}
