4fc07b69471c5ad545be9bd98b366108ba8128a3
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-24 16:38:30 -0600

    feat:se agregan los elementos de configuracion de la clase de dependencyinjection

--- Seph.Principal.Infraestructure/DependencyInjection/InfrastructureDependencyInjection.cs
+++ Seph.Principal.Infraestructure/DependencyInjection/InfrastructureDependencyInjection.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Domain.Repositories;
 using Seph.Principal.Infraestructure.Authentication;
+using Seph.Principal.Infraestructure.Authorization;
+using Seph.Principal.Infraestructure.Identity;
 using Seph.Principal.Infraestructure.Persistence;
+using System.Text;
 
 namespace Seph.Principal.Infraestructure.DependencyInjection
 {
@@ -17,6 +25,52 @@ namespace Seph.Principal.Infraestructure.DependencyInjection
 
             services.AddDbContext<ApplicationDbContext>(options => options.
                 UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+
+                options.Password.RequiredLength = 12;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+                options.User.RequireUniqueEmail = true;
+
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+
+                    options.RequireHttpsMetadata = true;
+                    options.SaveToken = false;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey)),
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.FromSeconds(30)
+                    };
+                });
+
+            services.AddAuthorizationBuilder()
+                .AddPolicy("Security.Admin", policy => policy.Requirements.Add(new PermissionRequirement("security.admin")))
+                .AddPolicy("Users.Read", policy => policy.Requirements.Add(new PermissionRequirement("users.read")));
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+            services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>());
+            services.AddScoped<IRefreshTokenSessionRepository, RefreshTokenSessionRepository>();
+            services.AddScoped<IIdentityService, IdentityService>();
+            services.AddSingleton<IJwtTokenService, JwtTokenService>();
+            services.AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>();
+
         }
     }
 }
