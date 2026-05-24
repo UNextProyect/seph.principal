19eba70d4cf455a709d20075c22bcef943d9eade
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-24 16:17:32 -0600

    feat: se agregan todos los elementos base de la arquitectura a trabajar con los procesos de la autenticacion del sustema.

--- Seph.Principal.Infraestructure/DependencyInjection/InfrastructureDependencyInjection.cs
+++ Seph.Principal.Infraestructure/DependencyInjection/InfrastructureDependencyInjection.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Infraestructure.Authentication;
+using Seph.Principal.Infraestructure.Persistence;
 
 namespace Seph.Principal.Infraestructure.DependencyInjection
 {
-    internal class InfrastructureDependencyInjection
+    public static class InfrastructureDependencyInjection
     {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtOptions>(configuration.GetSection(JwtOptions.SectionName));
+            var jwtOptions = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>() ??
+                throw new InvalidOperationException("La configuración  JWT es obligatoria");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.
+                UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
     }
 }
---
+++ Seph.Principal.Infraestructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Domain.Entities;
+using Seph.Principal.Infraestructure.Identity;
+
+namespace Seph.Principal.Infraestructure.Persistence
+{
+    public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options), IApplicationDbContext, IUnitOfWork
+    {
+        public DbSet<RefreshTokenSession> RefreshTokenSessions => Set<RefreshTokenSession>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+
+}
