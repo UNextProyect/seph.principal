fa8ed86177b9673e593611e061a5de9b26b1cb47
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-02 21:27:23 -0600

    fix:se agregan elementos para swagger extensions

--- Seph.Principal/Extensions/SwaggerExtensions.cs
+++ Seph.Principal/Extensions/SwaggerExtensions.cs
@@ -1,6 +1,46 @@
-﻿namespace Seph.Principal.Extensions
+﻿
+using Microsoft.OpenApi.Models;
+
+namespace Seph.Principal.Extensions
 {
-    public class SwaggerExtensions
+    public static class SwaggerExtensions
     {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Seph principal API",
+                    Version = "v1",
+                    Description = "API administrativa empresarial, Clean Architecture, JWT y CQRS."
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        []
+                    }
+                });
+            });
+
+            return services;
+        }
     }
 }
