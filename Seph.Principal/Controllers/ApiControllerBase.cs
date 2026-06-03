f880e5298d8bb6a2df879e5b53074d5f628303be
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-02 21:59:22 -0600

    feat:agregamos los elementos del controlador base

---
+++ Seph.Principal/Controllers/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Seph.Principal.Application.Common.Models;
+
+namespace Seph.Principal.Controllers
+{
+
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected IActionResult FromResponse<T>(ResponseWrapper<T> response)
+            => StatusCode((int)response.StatusCode, response);
+    }
+
+}
