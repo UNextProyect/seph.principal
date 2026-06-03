f499a7efd59928784e15ea435163691e75f06bef
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-03 00:34:29 -0600

    feat:se agrega  la entidad principal a trabajar en el flujo correcto,a demas de agregar el repositorio con las operaciones correctas.

---
+++ Seph.Principal.Domain/Entities/Institucion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Domain.Entities
+{
+    public class Institucion
+    {
+        public int Id { get; set; }
+
+        public string StrValor { get; set; } = string.Empty;
+
+        public string StrDescripcion { get; set; } = string.Empty;
+    }
+}
---
+++ Seph.Principal.Domain/Repositories/IInstitucionRepository.cs
@@ -0,0 +1,22 @@
+﻿using Seph.Principal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Domain.Repositories
+{
+    public interface IInstitucionRepository
+    {
+        Task<IReadOnlyList<Institucion>> GetAllAsync(CancellationToken cancellationToken);
+
+        Task<Institucion?> GetByIdAsync(int id,CancellationToken cancellationToken);
+
+        Task AddAsync(Institucion institucion,CancellationToken cancellationToken);
+
+        void Update(Institucion institucion);
+
+        void Delete(Institucion institucion);
+    }
+}
