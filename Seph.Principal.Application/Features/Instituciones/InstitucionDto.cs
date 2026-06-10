0df92b4928d2211da59c89827359177dfcf09164
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-10 12:02:46 -0600

    fix:se documenta clase para identificar sus elementos base.

--- Seph.Principal.Application/Features/Instituciones/InstitucionDto.cs
+++ Seph.Principal.Application/Features/Instituciones/InstitucionDto.cs
@@ -1,5 +1,13 @@
 ﻿namespace Seph.Principal.Application.Features.Instituciones
 {
+    /// <summary>
+    /// este es un record inmutable que se encarga de transportar los datos de la institucion, 
+    /// se utiliza para evitar problemas de concurrencia y para mejorar el rendimiento, 
+    /// ya que los records son inmutables y se pueden compartir entre hilos sin problemas de sincronización.
+    /// </summary>
+    /// <param name="Id">es el identificador de la institución</param>
+    /// <param name="StrValor">es el valor de la institución</param>
+    /// <param name="StrDescripcion">es la descripción de la institución</param>
     public sealed record InstitucionDto(
      int Id,
      string StrValor,
